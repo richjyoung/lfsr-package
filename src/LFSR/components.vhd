0272e27d6e46cb746ae20227e214386a81c03bbc
Rich Young <rich@richjyoung.co.uk>
2017-01-03 13:02:13 +0000

    Code tidying

--- src/LFSR/components.vhd
+++ src/LFSR/components.vhd
@@ -5,13 +5,13 @@ package components is
 
     component pulse is
         generic (
-            G_lfsr_width : natural := 3;
-            G_period : natural := 6
+            G_lfsr_width    : natural := 3;
+            G_period        : natural := 7
         );
         port(
-            CLK     : in  std_logic;
-            RESET   : in  std_logic;
-            PULSE   : out std_logic
+            CLK             : in  std_logic;
+            RESET           : in  std_logic;
+            PULSE           : out std_logic
         );
     end component;
 
--- src/LFSR/pulse.vhd
+++ src/LFSR/pulse.vhd
@@ -5,33 +5,34 @@ use LFSR.lfsr.all;
 --------------------------------------------------------------------------------
 entity pulse is
     generic (
-        G_lfsr_width : natural := 3;
-        G_period : natural := 6
+        G_lfsr_width    : natural := 3;
+        G_period        : natural := 7
     );
     port(
-        CLK     : in  std_logic;
-        RESET   : in  std_logic;
-        PULSE   : out std_logic
+        CLK             : in  std_logic;
+        RESET           : in  std_logic;
+        PULSE           : out std_logic
     );
 end pulse;
 --------------------------------------------------------------------------------
 architecture rtl of pulse is
-    constant C_ZERO : std_logic_vector(G_lfsr_width-1 downto 0) := (others => '0');
-    constant C_LFSR_RESET : std_logic_vector(G_lfsr_width-1 downto 0) := lfsr_eval(G_lfsr_width, G_period-1);
-    signal LFSR : std_logic_vector(G_lfsr_width-1 downto 0);
+    subtype T_LFSR          is std_logic_vector(G_lfsr_width-1 downto 0);
+    constant C_ZERO         : T_LFSR := (others => '0');
+    constant C_LFSR_RESET   : T_LFSR := lfsr_eval(G_lfsr_width, G_period-1);
+    signal LFSR             : T_LFSR;
 begin
 
-    PULSE <= '1' when LFSR = C_ZERO else '0';
+    PULSE                   <= '1' when LFSR = C_ZERO else '0';
 
-    pulse_proc: process (CLK) is
+    lfsr_proc: process (CLK) is
     begin
         if rising_edge(CLK) then
             if RESET = '1' then
-                LFSR        <= (others => '0');
+                LFSR        <= C_ZERO; -- XNOR LFSR requires zero reset
             else
                 lfsr_adv(LFSR, C_LFSR_RESET);
             end if;
         end if;
-    end process pulse_proc;
+    end process lfsr_proc;
 
 end rtl;
