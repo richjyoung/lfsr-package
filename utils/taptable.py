63a61c6fcfe41871b081e91af652938719fbd398
Rich Young <rich@richjyoung.co.uk>
2017-01-03 14:08:30 +0000

    Completed constants and script

---
+++ utils/taptable.py
@@ -0,0 +1,4 @@
+with open('xapp_052_table_3.csv','r') as csv:
+    for line in csv:
+        s = line.split(',');
+        print '({:>3}, {:>3}, {:>3}, {:>3}, {:>3}), -- {:>3}'.format(s[0], s[1], s[2], s[3], s[4].strip(), s[0])
