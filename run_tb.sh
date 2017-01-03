dae8dee7af07c494aa4e5e57b96e3f0110dc475b
Rich Young <rich@richjyoung.co.uk>
2017-01-03 00:55:19 +0000

    Updated Run TB

--- run_tb.sh
+++ run_tb.sh
@@ -2,20 +2,22 @@ CURR_DIR=$(pwd)
 LIBRARY=$1
 UUT=$2
 
+echo "==== Simulating $LIBRARY/$UUT ===="
+
 if [ -d "working" ]; then
     rm -rf working
 fi
-
 mkdir working
 cd working
 
+echo "Importing sources..."
 ghdl -i --work=LFSR ../src/LFSR/*.vhd
 ghdl -i --work=LFSR_TB ../test/LFSR_TB/*.vhd
-ghdl -m --work=$LIBRARY $UUT
+ghdl -m --work=$LIBRARY $UUT | tee make.log
 
-echo "Running Simulation..."
-ghdl -r $UUT --wave=$UUT.ghw
-echo "Simulation Complete."
+echo "Running simulation..."
+ghdl -r $UUT --wave=$UUT.ghw 2>&1 | tee sim.log
+echo "==== Simulation complete ===="
 
 gtkwave $UUT.ghw &
 
