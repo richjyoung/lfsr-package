307b2e1c31a106496a27ccef54f8b0534678fa08
Rich Young <rich@richjyoung.co.uk>
2017-01-10 22:21:59 +0000

    Removed reference to numeric_std

--- src/LFSR/pulse.vhd
+++ src/LFSR/pulse.vhd
@@ -1,6 +1,5 @@
 library IEEE, LFSR;
 use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
 use LFSR.lfsr.all;
 --------------------------------------------------------------------------------
 entity pulse is
